0d4287b75c9d2de55da4aed12d3056783186a9fd
Astolfi <67090311+Astolfi7@users.noreply.github.com>
2023-12-02 12:40:58 +0100

    Add files via upload

--- frontend/script.js
+++ frontend/script.js
@@ -5,3 +5,20 @@ function openPopup() {
 function closePopup() {
     document.getElementById("loginPopup").style.display = "none";
 }
+
+// Function to toggle the visibility of the dropdown content
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdownContent");
+    dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdownContent = document.getElementById("dropdownContent");
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      }
+    }
+  };
+  
